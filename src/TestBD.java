e98ed249c3c09a3478ce8a80e1a72b5ddc15c714
copernicus3001 <copernicus3001@o2.pl>
2020-01-14 21:15:16 +0100

    Next test commit

--- src/TestBD.java
+++ src/TestBD.java
@@ -4,6 +4,7 @@ public class TestBD {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("To jest test.");
+		System.out.println("A to jest drugi test");
 
 	}
 
